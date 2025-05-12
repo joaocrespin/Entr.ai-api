46b4724fc8b6a479f1ea7c906d4332c3d84b0371
joaocrespin <joaorato58@gmail.com>
2025-05-11 22:12:09 -0300

    feat: adiciona rotas de login e cadastro e model de usuário

--- app.py
+++ app.py
@@ -1,7 +1,54 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+from models.user import User, db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/meubanco'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
     return "Olá Flask"
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username or not password:
+        return "Nome de usuário e senha são obrigatórios!"
+    
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return 'Logado com sucesso'
+    else:
+        return 'Credenciais incorretas'
+
+
+@app.route("/cadastro", methods=["POST"])
+def signup():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not name or not password:
+        return "Nome de usuário e senha são obrigatórios!"
+    
+    if not email:
+        return "Email é obrigatório!"
+    
+    if User.query.filter_by(username=name).first():
+        return "Nome de usuário já existe!"
+    
+    user = User(username=name, email=email)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    return 'ok'
---
+++ models/user.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255), unique=True)
+
+
+    # TODO: resolver conflitos de tamanho no banco
+    def hash_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
